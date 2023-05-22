58f068f30d9e8cd12a1bbcd0ff9e3ee1757a79f5
Kevin Almeida <kevin.almeidafsa@gmail.com>
2023-05-22 16:25:25 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+function troggleMode() {
+  const html = document.documentElement;
+
+  //Forma Manual de fazer a troca de tema--------------------------------
+
+  //if (html.classList.contains("light")) {
+  //  html.classList.remove("light");
+  //}else{
+  //  html.classList.add("light");
+  // }
+
+  //---------------------------------------------------------------------
+
+  //Forma Automatizada de fazer a troca de tema--------------------------
+  html.classList.toggle("light");
+  //---------------------------------------------------------------------
+  //Mudando Img do perfil--------------------------------------------------
+
+  //Pegando Tag Img
+  const img = document.querySelector("#profileImg img");
+  //Condição de substituir a imagem
+  if (html.classList.contains("light")) {
+    //Se tiver no light mode, mudar img
+    img.setAttribute("src", "/assets/avatar-lightmode.png");
+    //Se tiver no light mode, mudar Alt
+    img.setAttribute("alt", "Avatar de kevin, com óculos no fundo verde");
+  } else {
+    //Se não tiver no light mode, mantenha img
+    img.setAttribute("src", "/assets/avatar-darkmode.png");
+    //Se não tiver no light mode, mantenha o Alt
+    img.setAttribute("alt", "Avatar de kevin, sem óculos no fundo verde");
+  }
+}
