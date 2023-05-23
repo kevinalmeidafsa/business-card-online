01a9f3d640e1ebe9a713479278c993f49f0c8835
Kevin Almeida <kevin.almeidafsa@gmail.com>
2023-05-23 11:37:02 -0300

    add path images

--- scripts/main.js
+++ scripts/main.js
@@ -21,12 +21,12 @@ function troggleMode() {
   //Condição de substituir a imagem
   if (html.classList.contains("light")) {
     //Se tiver no light mode, mudar img
-    img.setAttribute("src", "./assets/avatar-lightmode.png");
+    img.setAttribute("src", "../assets/avatar-lightmode.png");
     //Se tiver no light mode, mudar Alt
     img.setAttribute("alt", "Avatar de kevin, com óculos no fundo verde");
   } else {
     //Se não tiver no light mode, mantenha img
-    img.setAttribute("src", "./assets/avatar-darkmode.png");
+    img.setAttribute("src", "../assets/avatar-darkmode.png");
     //Se não tiver no light mode, mantenha o Alt
     img.setAttribute("alt", "Avatar de kevin, sem óculos no fundo verde");
   }
